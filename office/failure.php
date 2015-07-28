65c17dc6b7d510e66cfde4bcfb9525115564cc5f
Elia <ilya.razuvaev@hotmail.com>
2015-07-28 15:58:35 +0300

    Changing password error

---
+++ office/failure.php
@@ -0,0 +1,11 @@
+<?php ?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+<h1 style="color: crimson">Password changing has failed!</h1>
+</body>
+</html>
