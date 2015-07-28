23d73c6301536d7e69fcd4e9d17342b7e008022f
Elia <ilya.razuvaev@hotmail.com>
2015-07-28 16:02:08 +0300

    Lock script, if user is not logged in

---
+++ auth/lock.php
@@ -0,0 +1,19 @@
+<?php
+
+include '../configurations/config.php';
+
+session_start();
+
+$user_check = $_SESSION['login_user'];
+
+$ses_sql = mysqli_query($conn, "SELECT * FROM accounts WHERE login='".$user_check."'");
+
+$row = mysqli_fetch_assoc($ses_sql);
+
+$login_session = $row['login'];
+
+if(!isset($login_session)) {
+    header("Location: ../auth/login.php");
+    exit();
+}
+
