ffae691464cc28872d33715c7dc8c61d1d6bd295
Elia <ilya.razuvaev@hotmail.com>
2015-07-28 15:58:45 +0300

    Footer section

---
+++ categories/footer.php
@@ -0,0 +1,49 @@
+<style>
+    .footer-in-inner {
+        width: 1100px;
+        height: 300px;
+        margin: 0 auto;
+    }
+    .one, .two {
+        display: inline-block;
+        height: 300px;
+        width: 500px;
+        padding-top: 15px;
+        padding-left: 15px;
+    }
+    .two h2, .one h2 {
+        color: #b3a58a;
+        text-align: center;
+    }
+    ul li {
+        list-style: none;
+        line-height: 50px;
+        text-align: center;
+    }
+    .copy {
+        margin: 0 auto;
+        width: 270px;
+        padding-bottom: 10px;
+    }
+</style>
+<div class="footer-in-inner">
+    <div class="one">
+        <h2>Links</h2>
+        <ul>
+            <li><a href="">Link 1Link 1Link 1</a></li>
+            <li><a href="">Link 2Link 1Link 1</a></li>
+            <li><a href="">Link 3Link 1Link 1</a></li>
+            <li><a href="">Link 4Link 1Link 1</a></li>
+        </ul>
+    </div>
+    <div class="two">
+        <h2>Contacts</h2>
+        <ul>
+            <li><a href="">Link 1Link 1Link 1</a></li>
+            <li><a href="">Link 1Link 1Link 1</a></li>
+            <li><a href="">Link 1Link 1Link 1</a></li>
+            <li><a href="">Link 1Link 1Link 1</a></li>
+        </ul>
+    </div>
+    <div class="copy" style="font-size: 18px; color: #404040; font-family: 'Lucida Sans Unicode'">All rights reserved &copy; 2015</div>
+</div>
