2914d4543f4ab861c17b8f11e4ab0f22c85ecb41
Elia <ilya.razuvaev@hotmail.com>
2015-07-28 15:55:22 +0300

    About section

---
+++ categories/about.php
@@ -0,0 +1,78 @@
+<style>
+    .about-main-block {
+        width: 100%;
+        height: 100%;
+        color: #eba600;
+        margin-top: 20px;
+    }
+    .about-inner-block {
+        width: 1100px;
+        height: 100%;
+        margin: 0 auto;
+        padding: 7px;
+        background-color: #36372e;
+        opacity: 0.7;
+        border-radius: 10px;
+        border: 1px solid #bab894;
+        word-wrap: break-word;
+    }
+    #wr {
+        width:900px;
+        margin: 0 auto;
+        text-align: left;
+    }
+    .trigger,.trigger a {
+        display: block;
+    }
+    .cnt {
+        padding: 10px;
+        margin: 10px;
+    }
+    .cl {
+        width: 100%;
+        clear: both;
+        line-height: 0;
+        font-size: 0;
+    }
+    .trigger h2 {
+        color: #9a9b89;
+    }
+</style>
+
+<div class="about-main-block">
+    <div class="about-inner-block">
+
+        <div id="wr">
+            <div class="trigger open"><h2>Rates</h2></div>
+            <div class="cnt">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div class="cl">&nbsp;</div>
+
+            <div class="trigger open"><h2>Buffer</h2></div>
+            <div class="cnt">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div class="cl">&nbsp;</div>
+
+            <div class="trigger open"><h2>Olympiad</h2></div>
+            <div class="cnt">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div class="cl">&nbsp;</div>
+
+            <div class="trigger open"><h2>RaidBosses</h2></div>
+            <div class="cnt">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div class="cl">&nbsp;</div>
+
+            <div class="trigger open"><h2>Quests</h2></div>
+            <div class="cnt">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+            <div class="cl">&nbsp;</div>
+
+        </div>
+    </div>
+</div>
