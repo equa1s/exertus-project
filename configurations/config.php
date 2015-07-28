24a9bbc6eba01d7178dfc2affd3b0cdb127684b0
Elia <ilya.razuvaev@hotmail.com>
2015-07-28 16:22:50 +0300

    Update config.php

--- configurations/config.php
+++ configurations/config.php
@@ -8,16 +8,6 @@ $dbConnection = array(
     "database" => "l2ex_db"
 );
 
-// remote database
-
-/*$dbConnection = array(
-    "host" => "mysql.hostinger.com.ua",
-    "username" => "u176661300_root",
-    "password" => "L73RTwBEPl",
-    "database" => "u176661300_l2ex"
-);*/
-
-
 // Create connection
 $conn = mysqli_connect($dbConnection['host'],$dbConnection['username'],$dbConnection['password'],$dbConnection['database']);
 
